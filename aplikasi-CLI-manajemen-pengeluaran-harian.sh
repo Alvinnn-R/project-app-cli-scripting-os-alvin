2bc29eb6b294d7fdc8222b0afb8b4af662b8ee50
Alvinnn-R <alvinramasaputra03@gmail.com>
2025-11-16 06:10:27 +0700

    Create Mini Project 'Aplikasi CLI Manajemen Pengeluaran Harian'

---
+++ aplikasi-CLI-manajemen-pengeluaran-harian.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Aplikasi CLI Manajemen Pengeluaran Harian
+# Aplikasi terminal untuk mencatat pengeluaran harian, mengelola kategori, Filtering time range, dan melakukan analisis keuangan.
+
