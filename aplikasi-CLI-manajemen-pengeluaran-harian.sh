590cce876ec2e7fb3952e2e991b23fab9bbbfb03
Alvinnn-R <alvinramasaputra03@gmail.com>
2025-11-16 23:06:43 +0700

    fix: rename file mini project

--- aplikasi-CLI-manajemen-pengeluaran-harian.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Aplikasi CLI Manajemen Pengeluaran Harian
-# Aplikasi terminal untuk mencatat pengeluaran harian, mengelola kategori, Filtering time range, dan melakukan analisis keuangan.
-
